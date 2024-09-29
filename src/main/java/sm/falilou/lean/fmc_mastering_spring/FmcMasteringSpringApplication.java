c1410e9acc451502f55425040a1a7ad68673bffd
falilou cisse <cissefalilou8@gmail.com>
2024-09-29 18:22:39 +0000

    initial commit

---
+++ src/main/java/sm/falilou/lean/fmc_mastering_spring/FmcMasteringSpringApplication.java
@@ -0,0 +1,13 @@
+package sm.falilou.lean.fmc_mastering_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FmcMasteringSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FmcMasteringSpringApplication.class, args);
+	}
+
+}
